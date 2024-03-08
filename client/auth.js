4ac0b5226b76eefb218678a3639b511919fd5f3b
7326623 <capmrfremon@gmail.com>
2024-03-09 02:34:59 +0400

    структуры в reg и auth

--- client/auth.js
+++ client/auth.js
@@ -1,9 +1,17 @@
    function auth() {
-        console.log("reg fetch start!");
-        const response = await fetch('https://localhost/login');*/
-        if(true==true){
-         const data = await response.json();
-           window.location.href=mainpage.html
-        }
 
+    const object={         userName : document.getElementById(floatingInput),
+                           password : document.getElementById(floatingPassword) };
+
+           const response = await fetch('http://localhost:8080/auth/login',{
+
+                method: "POST",
+                body: JSON.stringify(object)
+           });
+
+           if(response==true){
+
+           const data = await response.json();
+              window.location.href ='mainpage.html';
+           }
     }
--- client/reg.js
+++ client/reg.js
@@ -1,10 +1,21 @@
    function reg() {
-        console.log("reg fetch start!");
-        const response = await fetch('http://localhost:8080/auth/registration');
-        if(response==true){
-         const data = await response.json();
 
-           console.log(data);
+        const object={  firstName :  document.getElementById(floatingName),
+                        lastName : "document.getElementById(floatingInput)",
+                        secondName : "document.getElementById(floatingInput)",
+                        email : document.getElementById(floatingInput),
+                        userName : document.getElementById(floatingLogin),
+                        password : document.getElementById(floatingPassword) };
+
+        const response = await fetch('http://localhost:8080/auth/registration',{
+
+             method: "POST",
+             body: JSON.stringify(object)
+        });
+        if(response==true){
+        const data = await response.json();
+
+           console.log(body);
            window.location.href ='mainpage.html';
         }
 
